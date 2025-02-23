f74563dcc871146ad62a4a114dd3500835ccf427
seorehnamuh <deborah.fazzuoli@istitutopantheon.it>
2025-02-23 16:02:04 +0100

    added UI  music and button sound

--- VideoGraphicProject/Assets/Scripts/PlayButton.cs
+++ VideoGraphicProject/Assets/Scripts/PlayButton.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class PlayButton : MonoBehaviour
 {
-    
+    AudioSource audioSource; // AudioSource che riproduce il suono
+    // [SerializeField] AudioClip clickSound;
    private string gameSceneName; 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         gameSceneName = "SampleScene";
     }
+
+
+
+
     void Update()
     {
         
@@ -18,6 +26,18 @@ public class PlayButton : MonoBehaviour
 
     public void LoadGame()
     {
-        SceneManager.LoadScene(gameSceneName);
+        PlaySound();
+        StartCoroutine(LoadSceneWithDelay());
     }   
+
+    private void PlaySound()
+    {
+        audioSource.Play();
+    }
+
+    private IEnumerator LoadSceneWithDelay()
+    {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(gameSceneName);
+    } 
 }
