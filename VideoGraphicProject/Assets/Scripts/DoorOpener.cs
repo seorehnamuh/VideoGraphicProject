4dc9986f4fc22af7e6aca4521b162732b4f8be1d
seorehnamuh <deborah.fazzuoli@istitutopantheon.it>
2025-03-01 16:18:24 +0100

    button opendoor + sound+ changecolor script added

---
+++ VideoGraphicProject/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject DoorCanvas;
+    [SerializeField] GameObject DoorToOpen;
+
+    private Animator DoorAnimator;
+    private AudioSource OpeningDoorSound; 
+    private bool IsTheDoorOpened;
+
+    private Material Material;
+    private Renderer Renderer;
+    void Start()
+    {
+        DoorCanvas.SetActive(false);
+        IsTheDoorOpened = false;
+        DoorAnimator = DoorToOpen.GetComponent<Animator>();
+        OpeningDoorSound = DoorToOpen.GetComponent<AudioSource>();
+        Renderer = GetComponent<Renderer>();
+        Material = Renderer.material;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player" && IsTheDoorOpened == false)
+        {
+            DoorCanvas.SetActive(true);
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            OpeningDoorSound.Play();
+            DoorAnimator.SetBool("OpenDoor", true);
+            IsTheDoorOpened = true;
+            Material.SetColor("_EmissionColor", Color.green);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        DoorCanvas.SetActive(false);
+    }
+
+}
