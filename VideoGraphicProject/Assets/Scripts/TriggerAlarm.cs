b5db42876b1a773033dbd77ce1060a2466eeb571
seorehnamuh <deborah.fazzuoli@istitutopantheon.it>
2025-04-13 12:56:13 +0200

    alarm + red light + instruction for defend script added.

---
+++ VideoGraphicProject/Assets/Scripts/TriggerAlarm.cs
@@ -0,0 +1,130 @@
+// using UnityEngine;
+
+// public class TriggerAlarm : MonoBehaviour
+// {
+//     public AudioSource alarmSound;
+//     public Light[] redLights;
+//     public float blinkSpeed = 1f;
+
+//     private bool alarmTriggered = false;
+//     private float blinkTimer = 0f;
+
+//     private void Start()
+//     {
+//         // Luci spente all'inizio
+//         foreach (Light light in redLights)
+//         {
+//             light.enabled = false;
+//         }
+//     }
+
+//     private void Update()
+//     {
+//         if (alarmTriggered)
+//         {
+//             BlinkLights();
+//         }
+//     }
+
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if (!alarmTriggered && other.CompareTag("Player"))
+//         {
+//             alarmTriggered = true;
+
+//             if (alarmSound != null && !alarmSound.isPlaying)
+//             {
+//                 alarmSound.Play();
+//             }
+//         }
+//     }
+
+//     private void BlinkLights()
+//     {
+//         blinkTimer += Time.deltaTime * blinkSpeed;
+//         bool isOn = Mathf.FloorToInt(blinkTimer) % 2 == 0;
+
+//         foreach (Light light in redLights)
+//         {
+//             light.enabled = isOn;
+//         }
+//     }
+// }
+
+
+using UnityEngine;
+using UnityEngine.UI;
+// Se usi TextMeshPro: using TMPro;
+
+public class TriggerAlarm : MonoBehaviour
+{
+    public AudioSource alarmSound;
+    public Light[] redLights;
+    public float blinkSpeed = 1f;
+
+    public GameObject alarmTextUI;
+
+    private bool alarmTriggered = false;
+    private float blinkTimer = 0f;
+
+    private void Start()
+    {
+        foreach (Light light in redLights)
+        {
+            light.enabled = false;
+        }
+
+        if (alarmTextUI != null)
+        {
+            alarmTextUI.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (alarmTriggered)
+        {
+            BlinkLights();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!alarmTriggered && other.CompareTag("Player"))
+        {
+            alarmTriggered = true;
+
+            if (alarmSound != null && !alarmSound.isPlaying)
+            {
+                alarmSound.loop = true;
+                alarmSound.Play();
+            }
+
+            if (alarmTextUI != null)
+            {
+                alarmTextUI.SetActive(true);
+                StartCoroutine(HideAlarmTextAfterDelay(5f));
+            }
+        }
+    }
+
+    private void BlinkLights()
+    {
+        blinkTimer += Time.deltaTime * blinkSpeed;
+        bool isOn = Mathf.FloorToInt(blinkTimer) % 2 == 0;
+
+        foreach (Light light in redLights)
+        {
+            light.enabled = isOn;
+        }
+    }
+
+    private System.Collections.IEnumerator HideAlarmTextAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (alarmTextUI != null)
+        {
+            alarmTextUI.SetActive(false);
+        }
+    }
+}
