5a9c7318da0acf3634728fe7334ed3d6907e643e
seorehnamuh <deborah.fazzuoli@istitutopantheon.it>
2025-04-14 18:38:17 +0200

    edited text in Assault Room - end added text "askR2D2 to open the door

--- VideoGraphicProject/Assets/Scripts/TriggerManager.cs
+++ VideoGraphicProject/Assets/Scripts/TriggerManager.cs
@@ -18,6 +18,7 @@ public class TriggerManager : MonoBehaviour
           { "TheBermudaTriangleTrigger", "The Bermuda Triangle - an ocean stretch has swallowed ships and planes for centuries. " },
           { "GravityAnomalyTrigger", " A gravity anomaly - a place where objects roll uphill, compasses spin erratically, people feel a force pulling them." },
           { "AccessDeniedTrigger", "ACCESS DENIED" },
+          { "OpenDoorRequest", "Request R2D2 to open the door" },
           { "Alien1Description", "Flying saucers streak across the night sky, defying gravity and vanishing in an instant." },
           { "Alien2Description", "Figures of light appear in the night. No faces, No features, just a pulsing radiance." },
           { "Alien3Description", "Classified recordings reveal impossible craft—defying physics, watching, unafraid." },
