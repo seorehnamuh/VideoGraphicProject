0b3f27e65de5a227a95002061bd1681aac817de4
seorehnamuh <deborah.fazzuoli@istitutopantheon.it>
2025-04-27 19:41:10 +0200

    added info to ask R2D2 to open the door + asset assault room + lights

--- VideoGraphicProject/Assets/Scripts/TriggerManager.cs
+++ VideoGraphicProject/Assets/Scripts/TriggerManager.cs
@@ -18,7 +18,7 @@ public class TriggerManager : MonoBehaviour
           { "TheBermudaTriangleTrigger", "The Bermuda Triangle - an ocean stretch has swallowed ships and planes for centuries. " },
           { "GravityAnomalyTrigger", " A gravity anomaly - a place where objects roll uphill, compasses spin erratically, people feel a force pulling them." },
           { "AccessDeniedTrigger", "ACCESS DENIED" },
-          { "OpenDoorRequest", "Request R2D2 to open the door" },
+          { "OpenDoorRequest", "ACCESS DENIED. Ask R2D2 to open the door" },
           { "Alien1Description", "Flying saucers streak across the night sky, defying gravity and vanishing in an instant." },
           { "Alien2Description", "Figures of light appear in the night. No faces, No features, just a pulsing radiance." },
           { "Alien3Description", "Classified recordings reveal impossible craft—defying physics, watching, unafraid." },
@@ -39,7 +39,7 @@ public class TriggerManager : MonoBehaviour
         string gameObjectTag = other.gameObject.tag;
         if (objectsWithMessages.ContainsKey(gameObjectTag))
         {
-            StartCoroutine(ShowMessage(gameObjectTag, 3));
+            StartCoroutine(ShowMessage(gameObjectTag, 5));
         }
 
     }
