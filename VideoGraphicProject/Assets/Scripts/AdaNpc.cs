04e1d2f6e8b3fd5c371e3b998d8b52e4f2e480b9
seorehnamuh <deborah.fazzuoli@istitutopantheon.it>
2025-04-27 14:48:43 +0200

    cancelled debug.log

--- VideoGraphicProject/Assets/Scripts/AdaNpc.cs
+++ VideoGraphicProject/Assets/Scripts/AdaNpc.cs
@@ -15,14 +15,14 @@ public class AdaNpc : MonoBehaviour
     {
         NpcDialogText.gameObject.SetActive(false);  
         notificationText.gameObject.SetActive(false);  
-        Debug.Log("AdaNpc script started.");  
+          
     }
 
     void Update()
     {
         if (isPlayerNear && Input.GetKeyDown(KeyCode.E) && canShowDialog)
         {
-            Debug.Log("Player pressed 'E'. Checking if dialog can be shown...");
+            
             HideNotification();  
             ShowDialog();  
         }
@@ -30,31 +30,29 @@ public class AdaNpc : MonoBehaviour
 
     private void ShowDialog()
     {
-        Debug.Log("Showing dialog with Ada.");
         NpcDialogText.gameObject.SetActive(true);  
         NpcDialogText.text = "Thank you, Sergeant Alfa. Check the Cockpit now."; 
     }
 
     private void ShowNotification()
     {
-        Debug.Log("Showing notification: 'Press E to talk to Ada'.");
+        
         notificationText.gameObject.SetActive(true);  
         notificationText.text = "Press [E] to talk to Ada.";  
     }
 
     private void HideNotification()
     {
-        Debug.Log("Hiding notification.");
+        
         notificationText.gameObject.SetActive(false);  
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Player collided with Ada.");
+        
         if (collision.gameObject.CompareTag("Player"))
         {
             isPlayerNear = true;  
-            Debug.Log("Player is near Ada.");
 
             if (instructionManager.GetCurrentPanelsChecked() == instructionManager.GetTotalPanels())
             {
@@ -67,7 +65,7 @@ public class AdaNpc : MonoBehaviour
    
     private void OnCollisionExit(Collision collision)
     {
-        Debug.Log("Player exited collision with Ada.");
+        
         if (collision.gameObject.CompareTag("Player"))
         {
             isPlayerNear = false;  
--- VideoGraphicProject/Assets/Scripts/DialogueNPC.cs
+++ VideoGraphicProject/Assets/Scripts/DialogueNPC.cs
@@ -1,111 +1,3 @@
-// using UnityEngine;
-// using TMPro;
-// using System.Collections;
-
-// public class DialogoNPC : MonoBehaviour
-// {
-//     public GameObject porta;
-//     public TMP_Text dialogoText;
-//     public TMP_Text notificationText;
-//     public string[] dialogoNPC;
-
-//     private bool nelRaggio = false;
-//     private bool dialogoAttivo = false;
-//     private int indiceDialogo = 0;
-
-//     private void Start()
-//     {
-//         dialogoText.gameObject.SetActive(false);
-//         notificationText.gameObject.SetActive(false);
-//     }
-
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         if (other.CompareTag("Player"))
-//         {
-//             Debug.Log("✅ Player è entrato nel trigger");
-//             nelRaggio = true;
-
-//             if (!dialogoAttivo)
-//             {
-//                 StartCoroutine(MostraMessaggioTemporaneo("Press [E] to interact", 2f));
-//             }
-//         }
-//     }
-
-//     private void OnTriggerExit(Collider other)
-//     {
-//         if (other.CompareTag("Player"))
-//         {
-//             Debug.Log("❌ Player è uscito dal trigger");
-//             nelRaggio = false;
-//             FineDialogo();
-//             notificationText.gameObject.SetActive(false);
-//         }
-//     }
-
-//     private void Update()
-//     {
-//         if (nelRaggio && Input.GetKeyDown(KeyCode.E))
-//         {
-//             Debug.Log("⌨️ Tasto E premuto");
-
-//             if (!dialogoAttivo)
-//             {
-//                 Debug.Log("🟡 Inizio dialogo");
-//                 notificationText.gameObject.SetActive(false);
-//                 IniziaDialogo();
-//             }
-//             else
-//             {
-//                 Debug.Log("➡️ Avanza dialogo");
-//                 AvanzaDialogo();
-//             }
-//         }
-//     }
-
-//     void IniziaDialogo()
-//     {
-//         dialogoAttivo = true;
-//         indiceDialogo = 0;
-//         dialogoText.gameObject.SetActive(true);
-//         dialogoText.text = dialogoNPC[indiceDialogo];
-//         Debug.Log("🗨️ Mostra frase: " + dialogoNPC[indiceDialogo]);
-//     }
-
-//     void AvanzaDialogo()
-//     {
-//         indiceDialogo++;
-
-//         if (indiceDialogo < dialogoNPC.Length)
-//         {
-//             dialogoText.text = dialogoNPC[indiceDialogo];
-//             Debug.Log("🗨️ Mostra frase: " + dialogoNPC[indiceDialogo]);
-//         }
-//         else
-//         {
-//             Debug.Log("🚪 Fine dialogo, disattivo porta");
-//             FineDialogo();
-//             porta.SetActive(false);
-//         }
-//     }
-
-//     void FineDialogo()
-//     {
-//         dialogoText.gameObject.SetActive(false);
-//         dialogoAttivo = false;
-//         Debug.Log("🔕 Dialogo chiuso");
-//     }
-
-//     IEnumerator MostraMessaggioTemporaneo(string messaggio, float durata)
-//     {
-//         notificationText.text = messaggio;
-//         notificationText.gameObject.SetActive(true);
-//         yield return new WaitForSeconds(durata);
-//         notificationText.gameObject.SetActive(false);
-//     }
-// }
-
 
 using UnityEngine;
 using TMPro;
@@ -113,13 +5,13 @@ using System.Collections;
 
 public class DialogoNPC : MonoBehaviour
 {
-    public GameObject porta;
-    public TMP_Text dialogoText;
-    public TMP_Text notificationText;
-    public string[] dialogoNPC;
+    [SerializeField] GameObject porta;
+    [SerializeField] TMP_Text dialogoText;
+    [SerializeField] TMP_Text notificationText;
+    [SerializeField] string[] dialogoNPC;
 
     // Nuove variabili per il suono
-    public AudioClip suonoAperturaPorta;  // Il suono da riprodurre
+    [SerializeField] AudioClip suonoAperturaPorta;  // Il suono da riprodurre
     private AudioSource audioSource;  // Il componente AudioSource
 
     private bool nelRaggio = false;
@@ -139,7 +31,6 @@ public class DialogoNPC : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("✅ Player è entrato nel trigger");
             nelRaggio = true;
 
             if (!dialogoAttivo)
@@ -153,7 +44,6 @@ public class DialogoNPC : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("❌ Player è uscito dal trigger");
             nelRaggio = false;
             FineDialogo();
             notificationText.gameObject.SetActive(false);
@@ -164,17 +54,15 @@ public class DialogoNPC : MonoBehaviour
     {
         if (nelRaggio && Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("⌨️ Tasto E premuto");
 
             if (!dialogoAttivo)
             {
-                Debug.Log("🟡 Inizio dialogo");
                 notificationText.gameObject.SetActive(false);
                 IniziaDialogo();
             }
             else
             {
-                Debug.Log("➡️ Avanza dialogo");
+                
                 AvanzaDialogo();
             }
         }
@@ -186,7 +74,7 @@ public class DialogoNPC : MonoBehaviour
         indiceDialogo = 0;
         dialogoText.gameObject.SetActive(true);
         dialogoText.text = dialogoNPC[indiceDialogo];
-        Debug.Log("🗨️ Mostra frase: " + dialogoNPC[indiceDialogo]);
+    
     }
 
     void AvanzaDialogo()
@@ -196,15 +84,15 @@ public class DialogoNPC : MonoBehaviour
         if (indiceDialogo < dialogoNPC.Length)
         {
             dialogoText.text = dialogoNPC[indiceDialogo];
-            Debug.Log("🗨️ Mostra frase: " + dialogoNPC[indiceDialogo]);
+        
         }
         else
         {
-            Debug.Log("🚪 Fine dialogo, disattivo porta");
+            
             FineDialogo();
             porta.SetActive(false);
 
-            // Riproduce il suono quando la porta viene disattivata
+    
             if (suonoAperturaPorta != null && audioSource != null)
             {
                 audioSource.PlayOneShot(suonoAperturaPorta);
@@ -216,7 +104,7 @@ public class DialogoNPC : MonoBehaviour
     {
         dialogoText.gameObject.SetActive(false);
         dialogoAttivo = false;
-        Debug.Log("🔕 Dialogo chiuso");
+
     }
 
     IEnumerator MostraMessaggioTemporaneo(string messaggio, float durata)
