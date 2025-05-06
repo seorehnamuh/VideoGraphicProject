c60dfa3f701fdf32d5e24c22c875f7c59dde11ee
seorehnamuh <deborah.fazzuoli@istitutopantheon.it>
2025-05-06 21:42:46 +0200

    added script for  muci table

--- VideoGraphicProject/Assets/Scripts/InstructionPlatform.cs
+++ VideoGraphicProject/Assets/Scripts/InstructionPlatform.cs
@@ -3,41 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class DoorInteraction : MonoBehaviour
+
+public class InstructionPlatform : MonoBehaviour
 {
     [SerializeField] TMP_Text interactionText; 
-   
-    public float displayDistance = 3f; 
+    
+    public float displayDistance = 3f;
 
-    private Transform playerTransform;
-    private bool isPlayerNear = false;
 
     void Start()
     {
-            interactionText.gameObject.SetActive(false);
+        interactionText.gameObject.SetActive(false);
     }
-
-
 
     void OnTriggerEnter(Collider other)
     {
-        
         if (other.CompareTag("Player"))
         {
-            
             interactionText.gameObject.SetActive(true);
-        
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        
         if (other.CompareTag("Player"))
         {
-           
             interactionText.gameObject.SetActive(false);
-            
         }
     }
 }
